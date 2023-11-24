3766674ba5ee50271957645c082fc00454177b11
Mona <monabadee@gmail.com>
2023-11-24 22:04:57 +0300

    Complete  Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+const { describe, beforeEach, it } = require('mocha');
+const { expect } = require('chai');
+const { Cat, Dog, Bird } = require('../path-to-your-classes-file');
+
+describe('Cat', () => {
+  let cat;
+
+  beforeEach(() => {
+    cat = new Cat('Whiskers', 'female');
+  });
+
+  it('has a class of cat', () => {
+    expect(cat).to.be.an.instanceOf(Cat);
+  });
+
+  it('can speak', () => {
+    expect(cat.speak()).to.equal('Whiskers says meow!');
+  });
+});
+
+describe('Dog', () => {
+  // Similar structure for Dog tests
+});
+
+describe('Bird', () => {
+  // Similar structure for Bird tests
+});
